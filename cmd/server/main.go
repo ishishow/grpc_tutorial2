e051b0fbe15cc72fabc0cc310545e4e56f2e6eb9
ishishow <ishikawa.sho.infratop@gmail.com>
2021-03-04 03:06:13 +0900

    サーバーポート変更

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	fmt.Println("Go gRPC Beginners Tutorial!")
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 51051))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
